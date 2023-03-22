1ce7421c89644588e14f7dd07fd6f14f5696ed04
Avijit Pateriya <deviljit.ap@gmail.com>
2023-03-22 19:16:40 +0530

    sample added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import SampleComp from "./components/SampleComp";
 function App() {
   return (
     <>
-      
+      <SampleComp />
     </>
   );
 }
---
+++ src/components/SampleComp.js
@@ -0,0 +1,10 @@
+import react from "react";
+const SampleComp = () => {
+  return (
+    <div>
+      <h1>hello this a sample for merge request</h1>
+    </div>
+  );
+};
+
+export default SampleComp;
